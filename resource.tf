600f9cd8a984ece378b8afc29763b3dd8879dec5
ilyashusain <ihusain1994@gmail.com>
2018-12-06 16:48:51 +0000

    terraform jenkins pipeline

---
+++ resource.tf
@@ -0,0 +1,41 @@
+resource "google_compute_instance" "default" {
+	name = "${var.name}"
+	machine_type = "${var.machine_type}"
+	zone = "europe-west2-c"
+	tags = ["jenkins"]
+	boot_disk {
+		initialize_params {
+			image = "${var.image}"
+		}
+	}
+	network_interface {
+		network = "${var.network}"
+		access_config {
+			// Ephemeral IP
+		}
+	}
+	metadata {
+    	sshKeys = "${var.ssh_user}:${file("~/.ssh/id_rsa.pub")}"
+  }	
+	connection = {
+		type = "ssh"
+		user = "terraform"
+		private_key = "${file("~/.ssh/id_rsa")}"
+	}
+	provisioner "remote-exec" {
+		inline = [
+			"sudo yum install -y java wget docker"
+		]
+	}
+	provisioner "remote-exec" {
+		script = "jenkins_centos7"
+	}
+	provisioner "remote-exec" {
+		script = "script_1"
+	}
+}
+
+resource "google_compute_disk" "attach_disk" {
+	name = "disk-2"
+	zone = "europe-west2-c"
+	}
